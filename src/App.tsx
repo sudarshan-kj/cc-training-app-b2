1903bc348cc33f7dd45c4da072ff949272e45860
KJ Sudarshan <kjsudi@gmail.com>
2021-11-26 07:19:35 +0530

    add cc-training-pp-b2

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+function getTitle() {
+  return "Hello React";
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <h1>{getTitle()}</h1>
+      <label htmlFor="search">Search</label>
+      <input id="search" />
+      {list.map((item) => (
+        <div>
+          <p>{item.title}</p>
+          <p>{item.url}</p>
+          <p>{item.author}</p>
+        </div>
+      ))}
     </div>
   );
 }
-
 export default App;
